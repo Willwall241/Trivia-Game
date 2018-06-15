5ac5dcc30147ee134c359acfe1e45dae20863882
WJW <will.j.waller@gmail.com>
2018-06-14 23:29:08 -0400

    update

--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -32,32 +32,33 @@ var questionArray = [
 
 var displayArray = ["That is Correct, Great Job!", "That is Incorrect!", "Uh Oh Times Up!", "Here is your Final Score!"]
 
-var currentQuestion=0; 
+var currentQuestion = 0;
 var correctAns = 0;
 var incorrectAns = 0;
 var unanswered = 0;
 var clicked = true;
 var time;
 var seconds;
+var userSelect;
 $("#restart").hide();
 
 //Start button click function
-$("#startGame").on("click", function() {
-//When start button is clicked
+$("#startGame").on("click", function () {
+  //When start button is clicked
   //call the startGame function
   startGame();
- 
+
 });
 
 //restart button click function
-$("#restart").on("click", function() {
+$("#restart").on("click", function () {
   //call the startGame function
   startGame();
-  
+
 });
 
 //startGame function
-function startGame(){
+function startGame() {
   //clear divs on the stage
   $("#startGame").hide();
   $("#restart").hide();
@@ -76,7 +77,7 @@ function startGame(){
 //startTimer function
 function startTimer() {
   //give seconds variable a value for time allotted
-  seconds = 25;
+  seconds = 5;
   //show seconds on the screen
   $("#timer").html("Time Remaining: " + seconds);
   //set an inteval to call the countdown function and set it to a variable
@@ -90,8 +91,10 @@ function countDown() {
   //update stage with new time
   $("#timer").html("Time Remaining: " + seconds);
   //if statement to end game if timer runs out and on last question
-  if (seconds < 0 && currentQuestion == questionArray.length) {
-    //final score function
+  if (seconds <= 0) {    
+    clicked = false;
+    evalAnswer();
+    clearInterval(time);
   }
 
 };
@@ -99,30 +102,70 @@ function countDown() {
 //loadQuestion function/answer onclick function
 function loadQuestion() {
   //push random question from array to stage
-  $("#question").text(questionArray[currentQuestion].question);
+  
 
-  //fill choices div with answerList as buttons
-  for(i=0;i<questionArray[currentQuestion].answerList.length;i++) {
-    var newButton = $("<button>");
-    newButton.text(questionArray[currentQuestion].answerList[i]);
-    newButton.addClass("choiceList");
-    newButton.attr("data-listNum", i);
-    $("#choices").append("<br>")
-    $("#choices").append(newButton);
-    $("#choices").append("<br>")
-    $("#choices").append("<br>")
+  if (currentQuestion <= questionArray.length - 1) {
+    $("#question").text(questionArray[currentQuestion].question);
+    //fill choices div with answerList as buttons
+    for (i = 0; i < questionArray[currentQuestion].answerList.length; i++) {
+      var newButton = $("<button>");
+      newButton.text(questionArray[currentQuestion].answerList[i]);
+      newButton.addClass("choiceList");
+      newButton.attr("data-listNum", i);
+      $("#choices").append("<br>")
+      $("#choices").append(newButton);
+      $("#choices").append("<br>")
+      $("#choices").append("<br>")
+    };
+    //call Timer function
+    startTimer();
+    //on click for buttons that will change page and evaluate answer
+    $(".choiceList").on("click", function () {
+      clicked = true;
+      userSelect = $(this).attr("data-listNum");
+      evalAnswer();
+      clearInterval(time);
+
+    });
   }
-  //call Timer function
-  startTimer();
-  //on click for buttons that will change page and evaluate answer
-  $(".choiceList").on("click", function(){
-    
-
-
-    clearInterval(time);
-  });
+  else {
+    //finalScore();
+  }
 };
 
-//function to evaluate guess
+  //function to evaluate guess
+  function evalAnswer() {
+    
+    $("#timer").empty();
+    $("#question").empty();
+    $("#choices").empty();
+
+    if (userSelect == questionArray[currentQuestion].correct && clicked == true) {
+      correctAns++;
+      currentQuestion++;
+      $("#message").text(displayArray[0]);
+      setTimeout(loadQuestion, 5000);
+    }
+    else if (userSelect != questionArray[currentQuestion].correct && clicked == true) {
+      incorrectAns++;
+      currentQuestion++;
+      $("#message").text(displayArray[1]);
+      setTimeout(loadQuestion, 5000);
+    }
+    else {
+      unanswered++;
+      currentQuestion++;
+      $("#message").text(displayArray[2]);
+      setTimeout(loadQuestion, 5000);
+    };
+    
+  };
 
 //function to show final score
+function finalScore() {
+  $("#messsage").empty();
+  $("#timer").empty();
+  $("#choices").empty();
+
+
+};
